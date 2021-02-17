7cb012974dc00e242e12938fc87cbccd8e7e912f
Sarah Molano <sarahmolano@sarahs-air-5.home>
2021-02-16 20:47:44 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function age_calculation(type, name, years){
+  return `My ${type}, ${name}, is ${years} (${years*12} months).`}
+
+age_calculation("mother", "Marlo", 47)
+// "My mother, Marlo, is 47 (564 months)."
+
+age_calculation("brother", "Matt", 22)
+// "My brother, Matt, is 22 (264 months)."
+
+age_calculation("sister", "Kaitlin", 20)
+// "My sister, Kaitlin, is 20 (240 months)."
